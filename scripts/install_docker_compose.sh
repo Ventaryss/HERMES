6f62152a0a0831011fc8797c23b665a9e9d9f38f
Venta <93933548+Ventaryss@users.noreply.github.com>
2024-07-18 08:24:20 +0200

    Update install_docker_compose.sh

--- scripts/install_docker_compose.sh
+++ scripts/install_docker_compose.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Installer Docker Compose si non installé
-if ! command -v docker compose &> /dev/null
+# Install Docker Compose if not already installed
+if ! command -v docker-compose &> /dev/null
 then
-    echo "Docker Compose non trouvé. Installation..."
+    echo "Docker Compose not found. Installing..."
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 else
-    echo "Docker Compose est déjà installé."
+    echo "Docker Compose is already installed."
 fi
