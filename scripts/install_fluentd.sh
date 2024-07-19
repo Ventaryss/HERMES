d0e0924deb2285ad850f94c55a126e7fc43d4a68
Venta <93933548+Ventaryss@users.noreply.github.com>
2024-07-19 09:17:17 +0200

    Update install_fluentd.sh

--- scripts/install_fluentd.sh
+++ scripts/install_fluentd.sh
@@ -20,6 +20,7 @@ cat <<EOL > ~/lpi-monitoring/configs/fluentd/fluent.conf
   </record>
 </filter>
 
+# Filtre pour pfSense
 <filter syslog.**>
   @type grep
   <regexp>
@@ -56,14 +57,19 @@ cat <<EOL > ~/lpi-monitoring/configs/fluentd/fluent.conf
   </match>
 </label>
 
+# Filtre pour les logs clients, hors pfSense et localhost
 <filter syslog.**>
   @type grep
   <regexp>
     key hostname
     pattern /^((?!pfSense).)*$/
   </regexp>
+</filter>
+
+<filter syslog.**>
+  @type grep
   <regexp>
-    key source_host
+    key hostname
     pattern /^((?!127.0.0.1).)*$/
   </regexp>
 </filter>
@@ -91,6 +97,56 @@ cat <<EOL > ~/lpi-monitoring/configs/fluentd/fluent.conf
     buffer_queue_limit 32
   </match>
 </label>
+
+# Filtre pour Stormshield
+<source>
+  @type syslog
+  port 24225
+  bind 0.0.0.0
+  tag stormshield
+</source>
+
+<filter stormshield.**>
+  @type parser
+  format syslog
+  time_format %b %d %H:%M:%S
+  key_name message
+</filter>
+
+<match stormshield.**>
+  @type loki
+  url http://loki:3100
+  extra_labels {"job":"stormshield"}
+  flush_interval 5s
+  flush_at_shutdown true
+  buffer_chunk_limit 1m
+  buffer_queue_limit 32
+</match>
+
+# Filtre pour Palo Alto
+<source>
+  @type syslog
+  port 24226
+  bind 0.0.0.0
+  tag paloalto
+</source>
+
+<filter paloalto.**>
+  @type parser
+  format syslog
+  time_format %b %d %H:%M:%S
+  key_name message
+</filter>
+
+<match paloalto.**>
+  @type loki
+  url http://loki:3100
+  extra_labels {"job":"paloalto"}
+  flush_interval 5s
+  flush_at_shutdown true
+  buffer_chunk_limit 1m
+  buffer_queue_limit 32
+</match>
 EOL
 
 # Use the specific docker-compose file for Fluentd
